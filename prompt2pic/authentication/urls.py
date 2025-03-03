7f6da96a389fe42002ed0be6b72893c188322691
Ohm Parmar <ohm3658@gmail.com>
2025-03-03 08:04:32 +0530

    minor change

--- prompt2pic/authentication/urls.py
+++ prompt2pic/authentication/urls.py
@@ -3,11 +3,9 @@ from . import views
 app_name = 'authentication'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),  # Add this line for login
+    path('login/', views.login, name='login'), 
     path('otp-verification/', views.otp_verification, name='otp_verification'),
     path('logout/', views.user_logout, name='logout'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
-    path('reset-password/', views.reset_password, name='reset_password'),  # New URL
-
-    # Add other authentication URLs here
+    path('reset-password/', views.reset_password, name='reset_password'), 
 ]
